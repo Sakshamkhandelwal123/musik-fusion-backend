55d8a8952a52b0553b2d79abbbfec0f8d4cc8949
Sakshamkhandelwal123 <sakshamkhandelwal2001@gmail.com>
2023-10-21 18:17:59 +0530

    Added basic / controller check

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import helmet from "helmet";
-import * as morgan from "morgan";
-import { NestFactory } from "@nestjs/core";
-import { ValidationPipe } from "@nestjs/common";
+import helmet from 'helmet';
+import * as morgan from 'morgan';
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 
-import { AppModule } from "./app.module";
-import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,12 +12,12 @@ async function bootstrap() {
   app.enableCors({
     origin: true,
     credentials: true,
-    allowedHeaders: ["Authorization", "Content-Type"],
+    allowedHeaders: ['Authorization', 'Content-Type'],
   });
 
   app.useGlobalPipes(new ValidationPipe());
 
-  app.use(morgan("combined"));
+  app.use(morgan('combined'));
 
   app.use(
     helmet({
@@ -25,31 +25,31 @@ async function bootstrap() {
         directives: {
           defaultSrc: [`'self'`],
           styleSrc: [`'self'`, `'unsafe-inline'`],
-          imgSrc: [`'self'`, "data:", "validator.swagger.io"],
+          imgSrc: [`'self'`, 'data:', 'validator.swagger.io'],
           scriptSrc: [`'self'`, `https: 'unsafe-inline'`],
         },
       },
       crossOriginEmbedderPolicy: false,
-    })
+    }),
   );
 
   const config = new DocumentBuilder()
-    .setTitle("Musik Fusion Apis")
-    .setDescription("These are musik fusion apis")
-    .setVersion("1.0")
-    .addTag("MusicFusionApis")
+    .setTitle('Musik Fusion Apis')
+    .setDescription('These are musik fusion apis')
+    .setVersion('1.0')
+    .addTag('MusicFusionApis')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("api-docs", app, document);
+  SwaggerModule.setup('api-docs', app, document);
 
-  const res = await app.listen(4000, "0.0.0.0");
+  const res = await app.listen(4000, '0.0.0.0');
   const serverAddress = res.address();
 
   console.log(
-    `⚡ Server is listening at http://${serverAddress.address}:${serverAddress.port}`
+    `⚡ Server is listening at http://${serverAddress.address}:${serverAddress.port}`,
   );
   console.log(
-    `⚡ Checkout Documentation at http://${serverAddress.address}:${serverAddress.port}/api-docs`
+    `⚡ Checkout Documentation at http://${serverAddress.address}:${serverAddress.port}/api-docs`,
   );
 }
 
