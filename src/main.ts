085a0bf872251cd7497b9196b6db0680af3bd7c4
Sakshamkhandelwal123 <sakshamkhandelwal2001@gmail.com>
2023-10-21 14:01:57 +0530

    Added swagger docs, eslint config, prettier config

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { NestFactory } from "@nestjs/core";
 import { ValidationPipe } from "@nestjs/common";
 
 import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -19,14 +20,36 @@ async function bootstrap() {
   app.use(morgan("combined"));
 
   app.use(
-    helmet({ contentSecurityPolicy: false, crossOriginEmbedderPolicy: false })
+    helmet({
+      contentSecurityPolicy: {
+        directives: {
+          defaultSrc: [`'self'`],
+          styleSrc: [`'self'`, `'unsafe-inline'`],
+          imgSrc: [`'self'`, "data:", "validator.swagger.io"],
+          scriptSrc: [`'self'`, `https: 'unsafe-inline'`],
+        },
+      },
+      crossOriginEmbedderPolicy: false,
+    })
   );
+
+  const config = new DocumentBuilder()
+    .setTitle("Musik Fusion Apis")
+    .setDescription("These are musik fusion apis")
+    .setVersion("1.0")
+    .addTag("MusicFusionApis")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api-docs", app, document);
 
   const res = await app.listen(4000, "0.0.0.0");
   const serverAddress = res.address();
 
   console.log(
     `⚡ Server is listening at http://${serverAddress.address}:${serverAddress.port}`
+  );
+  console.log(
+    `⚡ Checkout Documentation at http://${serverAddress.address}:${serverAddress.port}/api-docs`
   );
 }
 
