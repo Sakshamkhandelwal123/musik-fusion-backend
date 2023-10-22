9a2a5875c7eb17ec07481716971cfae79054aecb
Sakshamkhandelwal123 <sakshamkhandelwal2001@gmail.com>
2023-10-22 11:23:41 +0530

    Added users module

---
+++ src/users/dto/update-user.input.ts
@@ -0,0 +1,7 @@
+import { PartialType } from '@nestjs/mapped-types';
+
+import { CreateUserInput } from './create-user.input';
+
+export class UpdateUserInput extends PartialType(CreateUserInput) {
+  id: number;
+}
