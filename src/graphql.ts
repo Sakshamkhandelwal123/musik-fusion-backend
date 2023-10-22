f8d6b9455f5a8a66a2cc953b2916494012271a6a
Sakshamkhandelwal123 <sakshamkhandelwal2001@gmail.com>
2023-10-22 11:23:55 +0530

    Added graphql setup

---
+++ src/graphql.ts
@@ -0,0 +1,34 @@
+
+/*
+ * -------------------------------------------------------
+ * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
+ * -------------------------------------------------------
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+
+export interface CreateUserInput {
+    exampleField?: Nullable<number>;
+}
+
+export interface UpdateUserInput {
+    id: number;
+}
+
+export interface User {
+    exampleField?: Nullable<number>;
+}
+
+export interface IQuery {
+    users(): Nullable<User>[] | Promise<Nullable<User>[]>;
+    user(id: number): Nullable<User> | Promise<Nullable<User>>;
+}
+
+export interface IMutation {
+    createUser(createUserInput: CreateUserInput): User | Promise<User>;
+    updateUser(updateUserInput: UpdateUserInput): User | Promise<User>;
+    removeUser(id: number): Nullable<User> | Promise<Nullable<User>>;
+}
+
+type Nullable<T> = T | null;
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@ import helmet from 'helmet';
 import * as morgan from 'morgan';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
