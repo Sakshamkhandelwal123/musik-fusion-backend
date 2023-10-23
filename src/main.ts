1f26d5f00e2b0bf43e759ad31d35bc7b435b31a5
Sakshamkhandelwal123 <sakshamkhandelwal2001@gmail.com>
2023-10-23 10:51:50 +0530

    Updated variable names in main.ts

--- src/main.ts
+++ src/main.ts
@@ -33,17 +33,19 @@ async function bootstrap() {
     }),
   );
 
-  const config = new DocumentBuilder()
+  const swaggerConfig = new DocumentBuilder()
     .setTitle('Musik Fusion Apis')
     .setDescription('These are musik fusion apis')
     .setVersion('1.0')
     .addTag('MusicFusionApis')
     .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api-docs', app, document);
 
-  const res = await app.listen(4000, '0.0.0.0');
-  const serverAddress = res.address();
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('api-docs', app, swaggerDocument);
+
+  const response = await app.listen(4000, '0.0.0.0');
+  const serverAddress = response.address();
 
   console.log(
     `⚡ Server is listening at http://${serverAddress.address}:${serverAddress.port}`,
