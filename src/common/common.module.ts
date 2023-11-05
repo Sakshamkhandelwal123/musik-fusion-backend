4ab5ce656f8a4a2499b04ace807a142f6908b9e4
Sakshamkhandelwal123 <sakshamkhandelwal2001@gmail.com>
2023-11-05 18:31:42 +0530

    Added sendgrid service

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -3,11 +3,12 @@ import { Module, Global } from '@nestjs/common';
 import { DateScalar } from './scalar/date-scaler.service';
 import { SpotifyService } from './spotify/spotify.service';
 import { SpotifyController } from './spotify/spotify.controller';
+import { SendgridService } from './sendgrid/sendgrid.service';
 
 @Global()
 @Module({
-  providers: [SpotifyService, DateScalar],
+  providers: [SpotifyService, DateScalar, SendgridService],
   controllers: [SpotifyController],
-  exports: [SpotifyService, DateScalar],
+  exports: [SpotifyService, DateScalar, SendgridService],
 })
 export class CommonModule {}
