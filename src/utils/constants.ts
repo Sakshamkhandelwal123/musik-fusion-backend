af52e4451d02cee0ca9cc86107b11df781098359
Sakshamkhandelwal123 <sakshamkhandelwal2001@gmail.com>
2023-11-08 15:49:36 +0530

    Added password verification template

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,4 +5,5 @@ export const environment = {
 
 export const emailTemplates = {
   EMAIL_VERIFICATION: 'd-2a9a26bd36964f0abf20cbd6431b666d',
+  PASSWORD_VERIFICATION: 'd-084a8aac86cb4e43bf39e73febe73720',
 };
