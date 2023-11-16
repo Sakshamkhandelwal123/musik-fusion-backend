cc50aa9dd3db4ab24be4030578cf7378535f8d82
Sakshamkhandelwal123 <sakshamkhandelwal2001@gmail.com>
2023-11-16 15:12:55 +0530

    Added friend request table

--- database/migrations/20231109095403-friends-table.js
+++ database/migrations/20231109095403-friends-table.js
@@ -23,9 +23,39 @@ module.exports = {
         defaultValue: false,
         allowNull: false,
       },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      deleted_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+    });
+
+    await queryInterface.createTable('friend_requests', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      user_id: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      following_user_id: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
       friend_request_status: {
         type: Sequelize.ENUM('ACCEPTED', 'REJECTED', 'PENDING'),
-        allowNull: true,
+        allowNull: false,
+        defaultValue: 'PENDING',
       },
       created_at: {
         type: Sequelize.DATE,
@@ -44,5 +74,6 @@ module.exports = {
 
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable('friends');
+    await queryInterface.dropTable('friend_requests');
   },
 };
