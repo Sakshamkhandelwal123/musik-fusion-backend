41a11471f2003e629561400d76f80a1ecce02409
Sakshamkhandelwal123 <sakshamkhandelwal2001@gmail.com>
2023-11-21 15:15:35 +0530

    Added chat entity

--- src/chats/entities/chat.entity.ts
+++ src/chats/entities/chat.entity.ts
@@ -1,1 +1,47 @@
-export class Chat {}
+import {
+  Table,
+  Column,
+  Model,
+  PrimaryKey,
+  DataType,
+  CreatedAt,
+  UpdatedAt,
+  DeletedAt,
+} from 'sequelize-typescript';
+
+@Table({
+  underscored: true,
+})
+export class Chat extends Model {
+  @PrimaryKey
+  @Column({
+    type: DataType.UUID,
+    defaultValue: DataType.UUIDV4,
+    allowNull: false,
+  })
+  id: string;
+
+  @Column({ allowNull: false })
+  message: string;
+
+  @Column({ allowNull: false, defaultValue: false })
+  isWatched: boolean;
+
+  @Column({ allowNull: false })
+  channelId: string;
+
+  @Column({ allowNull: false })
+  userId: string;
+
+  @Column({ allowNull: false })
+  @CreatedAt
+  createdAt: Date;
+
+  @Column({ allowNull: false })
+  @UpdatedAt
+  updatedAt: Date;
+
+  @Column({ allowNull: true })
+  @DeletedAt
+  deletedAt: Date;
+}
