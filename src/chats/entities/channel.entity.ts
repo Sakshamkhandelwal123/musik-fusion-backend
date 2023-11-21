72a02a55753e749d850dbd56dee7f037bf493d79
Sakshamkhandelwal123 <sakshamkhandelwal2001@gmail.com>
2023-11-21 15:15:56 +0530

    Added channel entity

---
+++ src/chats/entities/channel.entity.ts
@@ -0,0 +1,44 @@
+import {
+  Table,
+  Column,
+  Model,
+  PrimaryKey,
+  DataType,
+  CreatedAt,
+  UpdatedAt,
+  DeletedAt,
+} from 'sequelize-typescript';
+
+@Table({
+  underscored: true,
+})
+export class Channel extends Model {
+  @PrimaryKey
+  @Column({
+    type: DataType.UUID,
+    defaultValue: DataType.UUIDV4,
+    allowNull: false,
+  })
+  id: string;
+
+  @Column({ allowNull: false })
+  name: string;
+
+  @Column({ allowNull: false })
+  createdBy: string;
+
+  @Column({ allowNull: true })
+  lastMessageTimestamp: Date;
+
+  @Column({ allowNull: false })
+  @CreatedAt
+  createdAt: Date;
+
+  @Column({ allowNull: false })
+  @UpdatedAt
+  updatedAt: Date;
+
+  @Column({ allowNull: true })
+  @DeletedAt
+  deletedAt: Date;
+}
