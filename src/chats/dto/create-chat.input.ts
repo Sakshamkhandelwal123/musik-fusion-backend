2678fe90ddb7c09989104b854028976ce302c31e
Sakshamkhandelwal123 <sakshamkhandelwal2001@gmail.com>
2023-11-22 18:38:59 +0530

    Added create message input

--- src/chats/dto/create-chat.input.ts
+++ src/chats/dto/create-chat.input.ts
@@ -1,4 +1,12 @@
-import { IsString } from 'class-validator';
+import { IsBoolean, IsOptional, IsString } from 'class-validator';
+
+export class CreateMessageInput {
+  @IsString()
+  message: string;
+
+  @IsString()
+  channelId: string;
+}
 
 export class CreateChatInput {
   @IsString()
@@ -6,4 +14,11 @@ export class CreateChatInput {
 
   @IsString()
   channelId: string;
+
+  @IsString()
+  userId: string;
+
+  @IsOptional()
+  @IsBoolean()
+  isWatched?: boolean;
 }
