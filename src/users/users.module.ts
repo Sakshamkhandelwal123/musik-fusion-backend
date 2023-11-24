f55196003ef8b962741f981a85f2e03b937e120a
Sakshamkhandelwal123 <sakshamkhandelwal2001@gmail.com>
2023-11-24 12:17:43 +0530

    Added user controller

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,11 +4,13 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 import { UsersResolver } from './users.resolver';
+import { UserController } from './users.controller';
 import { ChatsModule } from 'src/chats/chats.module';
 
 @Module({
   imports: [SequelizeModule.forFeature([User]), ChatsModule],
   providers: [UsersResolver, UsersService],
+  controllers: [UserController],
   exports: [UsersService],
 })
 export class UsersModule {}
