695fc71486cb3778576fd5a5eb9fa75b56a1f6e6
Sakshamkhandelwal123 <sakshamkhandelwal2001@gmail.com>
2023-11-24 15:55:09 +0530

    Added new apis

--- src/chats/dto/update-chat.input.ts
+++
@@ -1,6 +0,0 @@
-import { CreateChatInput } from './create-chat.input';
-import { PartialType } from '@nestjs/mapped-types';
-
-export class UpdateChatInput extends PartialType(CreateChatInput) {
-  id: number;
-}
