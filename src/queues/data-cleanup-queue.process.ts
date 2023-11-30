acae21d4369b2906cd0aeca50a2029ebf8f92ffe
Sakshamkhandelwal123 <sakshamkhandelwal2001@gmail.com>
2023-11-30 12:14:19 +0530

    Added basic data cleanup boilerplate

--- src/queues/data-cleanup-queue.process.ts
+++ src/queues/data-cleanup-queue.process.ts
@@ -1,7 +1,11 @@
 import { Job } from 'bullmq';
-import { Processor, WorkerHost } from '@nestjs/bullmq';
+import { HttpException, HttpStatus, Logger } from '@nestjs/common';
+import { OnWorkerEvent, Processor, WorkerHost } from '@nestjs/bullmq';
 
-import { queueNames } from 'src/utils/constants';
+import { User } from 'src/users/entities/user.entity';
+import { getErrorCodeAndMessage } from 'src/utils/helpers';
+import { Friend } from 'src/friends/entities/friend.entity';
+import { deleteEntity, queueNames } from 'src/utils/constants';
 
 @Processor(queueNames.DATA_CLEANUP_QUEUE)
 export class DataCleanupQueueProcessor extends WorkerHost {
@@ -9,5 +13,43 @@ export class DataCleanupQueueProcessor extends WorkerHost {
     super();
   }
 
-  async process(job: Job<unknown>): Promise<void> {}
+  async process(job: Job<unknown>): Promise<void> {
+    try {
+      const actionName = job.data['actionName'];
+      const actionData = job.data['actionData'];
+
+      switch (actionName) {
+        case deleteEntity.USER:
+          await this.deleteUserData(actionData);
+          break;
+        case deleteEntity.FRIEND:
+          await this.deleteFriendData(actionData);
+          break;
+      }
+    } catch (error) {
+      throw new HttpException(
+        getErrorCodeAndMessage(error),
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+
+  @OnWorkerEvent('active')
+  onActive(job: Job) {
+    Logger.log(`Processing job ${job.id}`);
+  }
+
+  @OnWorkerEvent('completed')
+  onComplete(job: Job) {
+    Logger.log(`Completed job of id ${job.id}`);
+  }
+
+  @OnWorkerEvent('failed')
+  async onError(job: Job, error: Error) {
+    Logger.error(`Failed job of id ${job.id} ${error}`);
+  }
+
+  async deleteUserData(data: User) {}
+
+  async deleteFriendData(data: Friend) {}
 }
