0c321bd98e3ba402961b34628ed9eb07a2dcf824
Sakshamkhandelwal123 <sakshamkhandelwal2001@gmail.com>
2023-12-12 16:12:58 +0530

    Added consumer group id and enums

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -31,41 +31,54 @@ export const kafkaClientId = {
       : `mf-events-process-${process.env.APP_ENV}`,
 };
 
+export const kafkaConsumerGroupId = {
+  consumerGroupId:
+    process.env.APP_ENV === environment.MAIN
+      ? 'mf-events-consumer'
+      : `mf-events-consumer-${process.env.APP_ENV}`,
+};
+
 export const kafkaTopics = {
   topic:
     process.env.APP_ENV === environment.MAIN
       ? {
           MUSIK_FUSION_USER_EVENTS: 'musik-fusion-user-events',
-          MUSIK_FUSION_CHANNEL_EVENTS: 'musik_fusion_channel_events',
+          MUSIK_FUSION_CHANNEL_EVENTS: 'musik-fusion-channel-events',
         }
       : {
           MUSIK_FUSION_USER_EVENTS: `musik-fusion-user-events-${process.env.APP_ENV}`,
-          MUSIK_FUSION_CHANNEL_EVENTS: `musik_fusion_channel_events-${process.env.APP_ENV}`,
+          MUSIK_FUSION_CHANNEL_EVENTS: `musik-fusion-channel-events-${process.env.APP_ENV}`,
         },
 };
 
-export const eventPerformers = {
-  USER: 'USER',
-};
+export enum KafkaTopic {
+  MUSIK_FUSION_USER_EVENTS = 'musik-fusion-user-events',
+  MUSIK_FUSION_CHANNEL_EVENTS = 'musik-fusion-channel-events',
+}
 
-export const entityTypes = {
-  USER: 'USER',
-};
+export enum EventPerformer {
+  USER = 'USER',
+  SYSTEM = 'SYSTEM',
+}
 
-export const eventNames = {
-  USER_SIGN_IN: 'USER_SIGN_IN',
-  USER_SIGN_UP: 'USER_SIGN_UP',
-  USER_PROFILE_UPDATED: 'USER_PROFILE_UPDATED',
-  USER_DELETED: 'USER_DELETED',
-  USER_FOLLOWED: 'USER_FOLLOWED',
-  USER_UNFOLLOWED: 'USER_UNFOLLOWED',
+export enum EntityType {
+  USER = 'USER',
+}
 
-  FRIEND_REQUEST_SENT: 'FRIEND_REQUEST_SENT',
-  FRIEND_REMOVED: 'FRIEND_REMOVED',
-  FRIEND_REQUEST_WITHDRAWN: 'FRIEND_REQUEST_WITHDRAWN',
-  FRIEND_REQUEST_ACCEPTED: 'FRIEND_REQUEST_ACCEPTED',
-  FRIEND_REQUEST_REJECTED: 'FRIEND_REQUEST_REJECTED',
+export enum EventName {
+  USER_SIGN_IN = 'USER_SIGN_IN',
+  USER_SIGN_UP = 'USER_SIGN_UP',
+  USER_PROFILE_UPDATED = 'USER_PROFILE_UPDATED',
+  USER_DELETED = 'USER_DELETED',
+  USER_FOLLOWED = 'USER_FOLLOWED',
+  USER_UNFOLLOWED = 'USER_UNFOLLOWED',
 
-  CHANNEL_JOINED: 'CHANNEL_JOINED',
-  CHANNEL_LEFT: 'CHANNEL_LEFT',
-};
+  FRIEND_REQUEST_SENT = 'FRIEND_REQUEST_SENT',
+  FRIEND_REMOVED = 'FRIEND_REMOVED',
+  FRIEND_REQUEST_WITHDRAWN = 'FRIEND_REQUEST_WITHDRAWN',
+  FRIEND_REQUEST_ACCEPTED = 'FRIEND_REQUEST_ACCEPTED',
+  FRIEND_REQUEST_REJECTED = 'FRIEND_REQUEST_REJECTED',
+
+  CHANNEL_JOINED = 'CHANNEL_JOINED',
+  CHANNEL_LEFT = 'CHANNEL_LEFT',
+}
