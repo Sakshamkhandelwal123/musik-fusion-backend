9e2a614e0d53f5d0f36e22755d1e58d5abd83d10
Sakshamkhandelwal123 <sakshamkhandelwal2001@gmail.com>
2023-12-13 15:00:40 +0530

    Added notifications migration

---
+++ database/migrations/20231213054012-notifications-table.js
@@ -0,0 +1,140 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('notifications', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      user_id: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      is_read: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      notification_type: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      entity_id: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      entity_type: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      deleted_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+    });
+
+    await queryInterface.createTable('notification_audiences', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      user_id: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      entity_id: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      entity_type: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      action: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      deleted_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+    });
+
+    await queryInterface.createTable('notification_meta', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      notification_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'notifications',
+          key: 'id',
+        },
+      },
+      entity_id: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      entity_type: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      reference_id: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      reference_type: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      metadata: {
+        type: Sequelize.JSONB,
+        allowNull: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      deleted_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('notifications');
+    await queryInterface.dropTable('notification_audiences');
+    await queryInterface.dropTable('notification_meta');
+  },
+};
