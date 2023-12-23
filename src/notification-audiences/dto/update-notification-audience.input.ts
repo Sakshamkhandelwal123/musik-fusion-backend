3915d410ae9562c0c79e0e64cc7bc94b2b83cc0f
Sakshamkhandelwal123 <sakshamkhandelwal2001@gmail.com>
2023-12-23 16:06:19 +0530

    Added input dtos

--- src/notification-audiences/dto/update-notification-audience.input.ts
+++
@@ -1,6 +0,0 @@
-import { CreateNotificationAudienceInput } from './create-notification-audience.input';
-import { PartialType } from '@nestjs/mapped-types';
-
-export class UpdateNotificationAudienceInput extends PartialType(CreateNotificationAudienceInput) {
-  id: number;
-}
--- src/notifications/dto/update-notification.input.ts
+++
@@ -1,6 +0,0 @@
-import { CreateNotificationInput } from './create-notification.input';
-import { PartialType } from '@nestjs/mapped-types';
-
-export class UpdateNotificationInput extends PartialType(CreateNotificationInput) {
-  id: number;
-}
